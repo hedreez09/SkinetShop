ed84688ac6899ab7852e31ebc503aa2808736206
Idris Wonuola <Idris.wonuola@wemabank.com>
2021-05-20 10:46:54 +0100

    Add project files.

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+	public class StoreContext:DbContext
+	{
+		public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+		{
+		}
+		public DbSet<Product> Products { get; set; }
+	}
+}

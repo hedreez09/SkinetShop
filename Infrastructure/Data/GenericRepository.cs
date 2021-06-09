0a662ab8ea8aea516c0640aa9b7942b36fe46dee
Idris Wonuola <Idris.wonuola@wemabank.com>
2021-06-09 04:39:04 +0100

    Implementing Specification pattern

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Core.Entity;
 using Core.Interface;
+using Core.Specifications;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,6 +27,25 @@ namespace Infrastructure.Data
 		public async Task<IReadOnlyList<T>> ListAllAsync()
 		{
 			return await _context.Set<T>().ToListAsync();
+		}
+
+
+		//ISpecification implemetation
+		public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+		{
+			return await ApplySpecification(spec).FirstOrDefaultAsync();
+		}
+
+		public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+		{
+			return await ApplySpecification(spec).ToListAsync();
+		}
+
+
+
+		private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+		{
+			return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
 		}
 	}
 }
---
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entity;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+	public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+	{
+		public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+		{
+			var query = inputQuery;
+
+			if(spec.Criteria != null)
+			{
+				query = query.Where(spec.Criteria);
+			}
+
+			// this help in cluding the brand and type when a product is search for in the db
+			query = spec.Includes.Aggregate(query, (current, Include) => current.Include(Include));
+
+			return query;
+		}
+	}
+}
